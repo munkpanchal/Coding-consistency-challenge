24b1b3386df78dc6773e36cbdf8ebf6ab1c83619
Mayank Panchal <mayankpanchal1801@gmail.com>
2024-04-08 16:06:24 +0530

    Day 6: Final update

---
+++ Day-6/longestSubstring.js
@@ -0,0 +1,11 @@
+var lengthOfLongestSubstring = function (s) {
+    let i = 0,
+        j = 0;
+
+    while (j < s.length) {
+        console.log(j);
+        j++;
+    }
+};
+let s = "abcabcbb";
+console.log(lengthOfLongestSubstring(s));
